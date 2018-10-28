e557e175f04951f451c0f3b499f60938b63d8354
Vladimir Shpegun <vladimirshpegun@Air-demo-3.WORKGROUP>
2018-10-28 10:14:29 +0200

    Initial commit

---
+++ src/com/hillel/hometask1/Calc.java
@@ -0,0 +1,10 @@
+package com.hillel.hometask1;
+
+public class Calc {
+    public static int add (int x, int y) {
+
+        int result = x + y ;
+        return result ;
+    }
+
+}
---
+++ src/com/hillel/hometask1/Starter.java
@@ -0,0 +1,11 @@
+package com.hillel.hometask1;
+
+public class Starter {
+
+    public static void main(String[] args) {
+
+        int r = Calc.add(2, 5) ;
+
+        System.out.println("x+y=" + r);
+    }
+}
